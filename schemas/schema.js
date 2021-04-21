8181854768c7c2978a528cc1fe5ce4c9129b632e
davaajantsan <davaajantsan@gerege.mn>
2021-04-21 23:32:23 +0800

    publisher bolson

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,16 +1,78 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      title: "Постууд",
+      name: "post",
+      type: "document",
+      fields: [
+        {
+          title: "Постын гарчиг",
+          name: "title",
+          type: "string",
+          description: "тайлбар 50 тэмдэгтээс хэтрэхгүй байх",
+        },
+        {
+          title: "Дэд гарчиг",
+          name: "subtitle",
+          type: "string",
+          description: "Постын төрөл текстээр бичнэ үү",
+        },
+        {
+          title: "Нийтлэгчийн нэр",
+          name: "publisher",
+          type: "reference",
+          to: [{ type: "publisher" }],
+        },
+        {
+          title: "Постын зураг",
+          name: "cover_image",
+          type: "image",
+        },
+        {
+          title: "Огноо",
+          name: "date",
+          type: "datetime",
+        },
+        {
+          title: "Хаяг",
+          name: "slug",
+          type: "slug",
+          options: {
+            source: "title",
+            maxLength: 200, // will be ignored if slugify is set
+            slugify: (input) =>
+              input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+          },
+        },
+      ],
+    },
+    {
+      title: "Нийтлэгч",
+      name: "publisher",
+      type: "document",
+      fields: [
+        {
+          title: "Нийтлэгчийн нэр",
+          name: "title",
+          type: "string",
+        },
+        {
+          title: "Нийтлэгчийн зураг",
+          name: "picture",
+          type: "image",
+        },
+      ],
+    },
   ]),
-})
+});
